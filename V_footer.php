6277210fbea0e23505bc1bbaa01b888f09378069
frilian2804 <101790101+frilian2804@users.noreply.github.com>
2022-04-08 15:56:01 +0700

    Add files via upload

---
+++ V_footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <a href="http://www.RentalBuku.com">Rental Buku</a>
+</footer>
+ </div>
---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Imam</li>
+        <li>Nama Belakang : Nawawi</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="square">
+     <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="triangle-right">
+     <li>Cirebon</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+     <li>Bulutangkis</li>
+     <li>Catur</li>
+ </ul>
+</section>
