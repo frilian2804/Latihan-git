63b657f7be1a37deae6c43e1229200db6ddbc655
frilian2804 <101790101+frilian2804@users.noreply.github.com>
2022-04-14 14:34:28 +0700

    Update Matakuliah.php

--- Matakuliah.php
+++ Matakuliah.php
@@ -12,8 +12,28 @@ class Matakuliah extends CI_Controller
 	public function cetak()
 	{
 		$data = ['kode' => $this->input->post('kode'),
+		$this->form_validation->set_rules('kode','Kode Matakuliah','required|min_length[3]',[
+			'required' => 'kode Matakuliah Harus diisi', 
+			'min_length' => 'Kode terlalu pendek'
+	]);
+
+		$this->form_validation->set_rules('nama','Nama Matakuliah','required|min_length[3]',[
+			'required' => 'Nama Matakuliah Harus diisi',
+			'min_length' => 'Nama terlalu pendek'
+	]);
+		if ($this->form_validation->run() != true)
+	{
+		$this->load->view('view-form-matakuliah');
+	} else {
+			$data = [
+				'kode' => $this->input->post('kode'),
 				'nama' => $this->input->post('nama'),
 				'sks' => $this->input->post('kode')];
 				$this->load->view('view-data-matakuliah', $data);
+				'sks' => $this->input->post('sks')
+			];
+
+			$this->load->view('view-data-matakuliah', $data);
+		}
 	}
 } 
